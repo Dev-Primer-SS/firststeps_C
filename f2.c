831d9de7ba8847c45acc0e17036267c58550def1
primeross <primeross@mail.ru>
2020-03-03 20:51:45 +0300

    with_Swhich_whthout_circle

---
+++ f2.c
@@ -0,0 +1,22 @@
+/*
+this is remaining ff.c file
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int val  = 0;
+char card_name[3];
+scanf("%2s",card_name);
+switch (card_name[0])
+{
+case 'A':
+	val = 11;
+    break;
+case 'Q':
+case 'K':
+case 'J':
+	val = 10;
+	break;
+default:
+    val = atoi(card_name);
+}
